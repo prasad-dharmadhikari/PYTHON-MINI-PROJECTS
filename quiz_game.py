15bac736745ec384d73d34df085b4174536b4b34
Prasad Dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2022-10-06 20:49:39 +0530

    ADDED : feature to display score at last

--- quiz_game.py
+++ quiz_game.py
@@ -3,28 +3,35 @@ playing = input("Do you want to play? ")
 
 if playing != "yes":
     quit()
-print("let's play")
+print("okay! let's play")
+score = 0
 
 answer = input("What does CPU stands for? ") 
 if answer.lower() == "central processing unit":
     print("Correct!")
+    score+=1
 else:
     print("Incorrect!")
 
 answer = input("What does RAM stands for? ") 
 if answer.lower() == "random access memory":
     print("Correct!")
+    score+=1
 else:
     print("Incorrect!")
 
 answer = input("What does GPU stands for? ") 
 if answer.lower() == "graphics processing unit":
     print("Correct!")
+    score+=1
 else:
     print("Incorrect!")
 
 answer = input("What does PSU stands for? ") 
 if answer.lower() == "power supply unit":
     print("Correct!")
+    score+=1
 else:
     print("Incorrect!")
+
+print("You got "+str(score)+" questions correct!!")
